cf69e614d80f45516e7484c0fec58e4858f11864
Ivan Milanov <ivan.milanov@netcetera.com>
2024-12-02 16:59:53 +0100

    Remove unnecessary comments from generated test files

--- src/test/java/org/superpranx/day02/RedNosedReactorAnalysisTest.java
+++ src/test/java/org/superpranx/day02/RedNosedReactorAnalysisTest.java
@@ -5,19 +5,9 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 public class RedNosedReactorAnalysisTest extends TestCase {
-
-  /**
-   * Create the test case
-   *
-   * @param testName name of the test case
-   */
   public RedNosedReactorAnalysisTest(String testName) {
     super(testName);
   }
-
-  /**
-   * @return the suite of tests being tested
-   */
   public static Test suite() {
     return new TestSuite(RedNosedReactorAnalysisTest.class);
   }
